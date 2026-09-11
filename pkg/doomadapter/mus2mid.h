d704d86b71a65a5edae90c181f0c892f45cca50d
Ian Shakespeare <ian@shakespeare.dev>
2026-09-11 15:01:05 -0600

    Add Go/cgo bindings for doomgeneric
    Vendors doomgeneric's core engine (no SDL/X11/audio backends) directly
    alongside the Go binding files in pkg/doomadapter, since cgo only
    compiles .c files colocated with the importing .go file.
    
    Public API: New, Tick, PushKey, Frame, KeyCode constants.

--- pkg/doomadapter/mus2mid.h
+++ pkg/doomadapter/mus2mid.h
@@ -1,1 +1,9 @@
-../../vendor/mus2mid.h
+#ifndef MUS2MID_H
+#define MUS2MID_H
+
+#include "doomtype.h"
+#include "memio.h"
+
+boolean mus2mid(MEMFILE *musinput, MEMFILE *midioutput);
+
+#endif /* #ifndef MUS2MID_H */
